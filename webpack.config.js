f4e3a9c03aca5367e30a5ab68865857c705d76d0
Vitaly Bryukhovetsky <bruhovet@list.ru>
2020-02-20 06:57:06 +0300

    Правит пути копирования изображений

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
           {
             loader: `file-loader`,
             options: {
-              name: `../img/[name].[ext]`,
+              name: `img/[name].[ext]`,
               publicPath: `../`
             },
           },
